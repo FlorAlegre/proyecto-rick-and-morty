d3dd00a5a6a59eabb51a7b287ff7b4c6ca60cd76
Flor Alegre <floralegre94@gmail.com>
2023-03-24 15:22:02 -0300

    Add API

---
+++ app.js
@@ -0,0 +1,31 @@
+
+const llamadaAApi = fetch("https://rickandmortyapi.com/api/character");
+
+llamadaAApi
+    .then( (data)=> {
+     return data.json();
+    })
+    .then((data)=>{
+        const $container = document.getElementById("container");
+        const character = data.results;
+        console.log(character[0]);
+        for(let i = 0; i < character.length; i++) {
+            $container.innerHTML += `
+            <div class="item-grid">
+                <img 
+                    src= ${character[i].image} 
+                    alt="character imagen"
+                />
+                <h2>${character[i].name}</h2>
+                <p>Gender: ${character[i].gender}</p> 
+                <p>Species: ${character[i].species}</p>
+                <p>Origin: ${character[i].origin.name}
+                <p>Status: ${character[i].status}</p>
+                
+            </div>
+            `;
+        }
+    })
+    .catch((err) => {
+        console.log(err);
+    });
